0ea877f730e0f616b4007b89fb0e49fdfd05ed4c
shawn <mingyuemyheart@126.com>
2019-08-02 18:05:37 +0800

    增加实况监测新版

--- hLJDecision/src/main/java/com/hlj/activity/HMainActivity.java
+++ hLJDecision/src/main/java/com/hlj/activity/HMainActivity.java
@@ -71,8 +71,8 @@ public class HMainActivity extends BaseFragmentActivity implements OnClickListen
 	 * 初始化控件
 	 */
 	private void initWidget() {
-//		AutoUpdateUtil.checkUpdate(HMainActivity.this, mContext, "41", getString(R.string.app_name), true);//黑龙江气象
-		AutoUpdateUtil.checkUpdate(HMainActivity.this, mContext, "53", getString(R.string.app_name), true);//决策气象服务
+		AutoUpdateUtil.checkUpdate(HMainActivity.this, mContext, "41", getString(R.string.app_name), true);//黑龙江气象
+//		AutoUpdateUtil.checkUpdate(HMainActivity.this, mContext, "53", getString(R.string.app_name), true);//决策气象服务
 
 		ivSetting = (ImageView) findViewById(R.id.ivSetting);
 		ivSetting.setOnClickListener(this);
